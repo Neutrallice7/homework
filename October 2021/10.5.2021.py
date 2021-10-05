bd7de97dc7c46b9c393810edd003521e65b048f2
Neutrallice7 <sulthanprabowo7@gmail.com>
2021-10-05 18:32:04 +0700

    Create 10.5.2021.py

---
+++ October 2021/10.5.2021.py
@@ -0,0 +1,21 @@
+x = int(input("input number:"))
+
+if x % 11 == 0:
+    print("a")
+else:
+    print("e")
+
+if x % 9 == 0:
+    print("b")
+else:
+    print("e")
+
+if x % 7 == 0:
+    print("c")
+else:
+    print("e")
+
+if x % 2 == 0:
+    print("d")
+else:
+    print("e")
