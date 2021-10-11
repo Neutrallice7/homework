1993e34da56b2acbf3489bf8a375a278a4acbcd9
Neutrallice7 <sulthanprabowo7@gmail.com>
2021-10-11 15:35:33 +0700

    Create DiscountSulthan.py

---
+++ October 2021/PythonExercise2/DiscountSulthan.py
@@ -0,0 +1,17 @@
+Quantity = float(input('Enter the number of packages that are bought:'))
+
+if Quantity < 10:
+    Discount = 0
+if 10 <= Quantity <= 19:
+    Discount = 10
+if 20 <= Quantity <= 49:
+    Discount = 20
+if 50 <= Quantity <= 99:
+    Discount = 30
+if Quantity >= 100:
+    Discount = 40
+
+Discounted = (99*Quantity) * Discount/100
+Total = (99*Quantity) - Discounted
+
+print('Total amount: $',Total)
