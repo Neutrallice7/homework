cbe6b8d129c1983827c3cdfc03f0dd3c2d0ee212
Neutrallice7 <sulthanprabowo7@gmail.com>
2021-10-26 11:39:17 +0700

    Update Num1.py

--- October 2021/Functional Programming/Num1.py
+++ October 2021/Functional Programming/Num1.py
@@ -6,5 +6,5 @@ def convert_to_days(): #functions
     seconds = float(input('Enter seconds: '))
     return (hours / 24), ((minutes /60) / 24), (((seconds / 60) / 60) / 24) #conversion to days
 
-x,y,z = convert_to_days()
+x,y,z = convert_to_days()  
 print('{:.4f}'.format(x + y + z)) #format to 4 significant figures
