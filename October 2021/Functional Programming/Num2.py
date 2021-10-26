01645748738c2666d4b77c3ddd2b6873e660bfa0
Neutrallice7 <sulthanprabowo7@gmail.com>
2021-10-26 12:13:45 +0700

    Update Num2.py

--- October 2021/Functional Programming/Num2.py
+++ October 2021/Functional Programming/Num2.py
@@ -2,12 +2,12 @@ import math
 
 # weight = mass * gravity 
 
-# try to find the mass in Earth -> use that mass to convert it to Jupiter??
+# try to find the mass in Earth -> use that mass to convert it to the weight in Jupiter
 
-def calc_weight_on_planet(weight=120, gravity=23.1):
-    mass = weight / 9.8
-    newWeight = mass * gravity
+def calc_weight_on_planet(weight=120, gravity=23.1): #default values
+    mass = weight / 9.8 #converts weight to mass in Earth
+    newWeight = mass * gravity #converts mass to weight in Jupiter
     return newWeight
 
-x = calc_weight_on_planet()
-print(x)
+x = calc_weight_on_planet() 
+print(x) #prints the result
