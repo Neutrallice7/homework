994b38ed108a211d5e0f408cfdacbb2b929cc321
Sulthan Syuza Prabowo <86139250+Neutrallice7@users.noreply.github.com>
2021-10-26 21:48:00 +0700

    Update Num5.py

--- October 2021/Functional Programming/Num5.py
+++ October 2021/Functional Programming/Num5.py
@@ -1,13 +1,13 @@
-def convert_temp():
-    F = float(input('Insert temperature in Fahrenheit: '))
-    C = 5/9 * ( F - 32 )
+def convert_temp(): 
+    F = float(input('Insert temperature in Fahrenheit: ')) #user input
+    C = 5/9 * ( F - 32 ) #formulas to convert
     K = C + 273.15
     return F,C,K
 
 
-F,C,K = convert_temp()
+F,C,K = convert_temp() #functions
 
-print('The temperature in Fahrenheit is', F)
+print('The temperature in Fahrenheit is', F) #prints output
 print('The temperature in Celsius is', C)
 print('The temperature in Kelvin is', K)
         
