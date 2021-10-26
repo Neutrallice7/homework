e08fc276d1fc1a8ac6a121255c59dcb892cd0dec
Neutrallice7 <sulthanprabowo7@gmail.com>
2021-10-26 20:08:30 +0700

    Create Num3.py

---
+++ October 2021/Functional Programming/Num3.py
@@ -0,0 +1,12 @@
+
+
+# this is for reference print((10/196.97) * 6.0221409e+23)
+
+def num_atoms(weight=196.97):
+    atoms = ((10 / weight) * 6.0221409e+23)
+    return atoms
+
+x = num_atoms(1.008)
+print(x)
+
+
