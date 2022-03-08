b4079f5e30f6c85e5425a4d43baac1954d4aa166
Sulthan Syuza Prabowo <86139250+Neutrallice7@users.noreply.github.com>
2022-03-08 20:52:11 +0700

    Added Number8

---
+++ March 2022/08.03.22/Number8.java
@@ -0,0 +1,14 @@
+public class Number8 {
+    public static void main(String[] args)
+    {
+        System.out.println("  *********     ***       *        *    \n");
+        System.out.println("  *       *   *     *    ***      * *   \n");
+        System.out.println("  *       *  *       *  *****    *   *  \n");
+        System.out.println("  *       *  *       *    *     *     * \n");
+        System.out.println("  *       *  *       *    *    *       *\n");
+        System.out.println("  *       *  *       *    *     *     * \n");
+        System.out.println("  *       *  *       *    *      *   *  \n");
+        System.out.println("  *       *   *     *     *       * *   \n");
+        System.out.println("  *********     ***       *        *    \n");
+    }
+}
