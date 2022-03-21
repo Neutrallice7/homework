e5865eb2f0bf2c4d2cc56547c26c458b5ce69f99
Sulthan Syuza Prabowo <86139250+Neutrallice7@users.noreply.github.com>
2022-03-21 16:42:52 +0700

    Upload

--- March 2022/21.03.22/DataStructures.cpp
+++ March 2022/21.03.22/DataStructures.cpp
@@ -1,23 +1,20 @@
-#include<iostream>
-#include<conio.h>
-#include<fstream>
-using namespace std;
+#include <iostream>
+#include <conio.h>
+#include <fstream>
 
-string fname,lname,phone_num;
+using namespace std;
 
 void ContactAdd();
 void ContactSearch();
-void self_exit();
-bool check_digits(string);
-bool check_numbers(string);
+void exitContact();
+bool digitCheck();
+bool numCheck();
 
 int main()
 {
     short int choice;
-    system("cls");
-    system("color 0A");
-    cout << "\n\n\n\t\t\tContact Management.";
-    cout << "\n\n\t1. Add Contact\n\t2. Search Contact\n\t3. Exit\n\t> ";
+    cout << "Contact Management.";
+    cout << "\n1. Add Contact \n2. Search Contact\n3. Exit";
     cin >> choice;
 
     switch(choice)
@@ -27,109 +24,23 @@ int main()
             break;
         case 2:
             ContactSearch();
-            break;
         case 3:
-            self_exit();
+            exitContact();
             break;
         default:
-            cout << "\n\n\tInvalid Input !";
-            cout << "\n\tPress Any Key To Continue..";
             getch();
             main();
     }
     return 0;
-}
+};
 
-void self_exit()
+void exitContact()
 {
-    system("cls");
-    cout << "\n\n\n\t\tThank You For Using !";
+    cout << "Program end.";
     exit(1);
 }
+
 void ContactAdd()
 {
-    ofstream phone("number.txt", ios::app);
-    system("cls");
-    cout << "\n\n\tEnter First Name : ";
-    cin >> fname;
-    cout << "\n\tEnter Last Name : ";
-    cin >> lname;
-    cout << "\n\tEnter 10-Digit Phone Number : ";
-    cin >> phone_num;
-
-    if(check_digits(phone_num) == true)
-    {
-        if(check_numbers(phone_num) == true)
-        {
-            if(phone.is_open())
-            {
-                phone << fname << " " << lname << " " << phone_num << endl;
-                cout << "\n\tContact Saved Successfully !";
-            }
-            else
-            {
-                cout << "\n\tError Opening File !";
-            }
-        }
-        else
-        {
-            cout << "\n\tA Phone Number Must Contain Numbers Only !";
-        }
-    }
-    else
-    {
-        cout << "\n\tA Phone Number Must Contain 10 Digits.";
-    }
-    phone.close();
-}
-
-void ContactSearch()
-{
-    bool found = false;
-    ifstream myfile("number.txt");
-    string keyword;
-    cout << "\n\tEnter Name To Search : ";
-    cin >> keyword;
-    while(myfile >> fname >> lname >> phone_num)
-    {
-        if(keyword == fname || keyword == lname)
-        {
-            system("cls");
-            cout << "\n\n\n\t\tContact details..";
-            cout << "\n\n\tFirst Name : " << fname;
-            cout << "\n\tLast Name : " << lname;
-            cout << "\n\tPhone Number : " << phone_num;
-            found = true;
-            break;
-        }
-    }
-    if(found == false)
-    cout << "\n\tNo Such Contact Found";
-}
-
-bool check_digits(string x)
-{
-    if(x.length() == 10)
-    return true;
-    else
-    return false;
-}
-
-bool check_numbers(string x)
-{
-    bool check = true;
-
-    for(int i=0; i < x.length(); i++)
-    {
-        if(!(int(x[i]) >= 48 && int(x[i]) <= 57))
-        {
-            check = false;
-            break;
-        }
-    }
-
-    if(check == true)
-    return true;
-    if(check == false)
-    return false;
+    
 }
