9047c0259e39ef96e48ce2b2ad9f73f43c808efc
Sulthan Syuza Prabowo <86139250+Neutrallice7@users.noreply.github.com>
2022-03-21 17:37:06 +0700

    Update DataStructures.cpp

--- March 2022/21.03.22/DataStructures.cpp
+++ March 2022/21.03.22/DataStructures.cpp
@@ -1,14 +1,15 @@
-#include <iostream>
-#include <conio.h>
-#include <fstream>
-
+#include<iostream>
+#include<conio.h>
+#include<fstream>
 using namespace std;
+
+string name, phoneNum;
 
 void ContactAdd();
 void ContactSearch();
 void exitContact();
-bool digitCheck();
-bool numCheck();
+bool digitCheck(string);
+bool numCheck(string);
 
 int main()
 {
@@ -24,6 +25,7 @@ int main()
             break;
         case 2:
             ContactSearch();
+            break;
         case 3:
             exitContact();
             break;
@@ -32,7 +34,7 @@ int main()
             main();
     }
     return 0;
-};
+}
 
 void exitContact()
 {
@@ -42,5 +44,83 @@ void exitContact()
 
 void ContactAdd()
 {
-    
+    ofstream phone("database.txt", ios::app);
+    cout << "Enter name: ";
+    cin >> name;
+    cout << "Enter phone numer (10 digits): ";
+    cin >> phoneNum;
+
+    if (digitCheck(phoneNum) == true)
+    {
+        if(numCheck(phoneNum) == true)
+        {
+            if(phone.is_open())
+            {
+                phone << name << " " << phoneNum << endl;
+                cout << "\nSaved.";
+            }
+            else
+            {
+                cout << "\n Error";
+            }
+        }
+        else
+        {
+            cout << "\nMust contain numbers only.";
+        }
+    }
+    else
+    {
+        cout << "\nMust contain 10 digits.";
+    }
+    phone.close();
+}
+
+void ContactSearch()
+{
+    bool found = false;
+    ifstream myfile("database.txt");
+    string keyword;
+    cout << "\n\tEnter Name To Search : ";
+    cin >> keyword;
+    while(myfile >> name >> phoneNum)
+    {
+        if(keyword == name)
+        {
+            cout << "\n\n\n\t\tDetails";
+            cout << "\nName : " << name;
+            cout << "\n\tPhone Number : " << phoneNum;
+            found = true;
+            break;
+        }
+    }
+    if(found == false)
+    cout << "\n\tNone exist";
+}
+
+bool digitCheck(string x)
+{
+    if(x.length() == 10)
+    return true;
+    else
+    return false;
+}
+
+bool numCheck(string x)
+{
+    bool check = true;
+
+    for(int i=0; i < x.length(); i++)
+    {
+        if(!(int(x[i]) >= 48 && int(x[i]) <= 57))
+        {
+            check = false;
+            break;
+        }
+    }
+
+    if(check == true)
+    return true;
+    if(check == false)
+    return false;
 }
